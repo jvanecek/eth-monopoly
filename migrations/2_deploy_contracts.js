2bd72fe231cfa558f526e6a975819e932c75b886
juan2511 <juann.vanecek@gmail.com>
2017-11-27 20:25:12 -0300

    Configuro testrpc para travis

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,11 @@
+var Banker = artifacts.require('./Banker.sol');
+var MonopolyBoard = artifacts.require('./MonopolyBoard.sol');
+var MonopolyGame = artifacts.require('./MonopolyGame.sol');
+
+module.exports = function(deployer) {
+  deployer.deploy(Banker).then(async () => {
+    deployer.deploy(MonopolyBoard, Banker.address).then(async () => {
+      await deployer.deploy(MonopolyGame, Banker.address, MonopolyBoard.address);
+    })
+  });
+}
